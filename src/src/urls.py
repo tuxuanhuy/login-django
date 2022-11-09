f5c149b84b06541400dc18bd33b8745deaa20f28
tuxuanhuy <68751002+tuxuanhuy@users.noreply.github.com>
2022-11-09 21:58:57 +0700

    Login form

--- src/src/urls.py
+++ src/src/urls.py
@@ -19,10 +19,17 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+from first.views import UserViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+router = DefaultRouter()
+router.register('user', UserViewSet, basename='user')
+
+urlpatterns += router.urls
